7db61b625d0ee42a712e2b2457c756f9f0175791
mahdi <mahdighaleb196@>
2025-03-25 22:24:10 -0700

    vite render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,9 +8,15 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
+        target: 'http://localhost:3001', 
+        changeOrigin: true, 
+        secure: false,
+      },
+    },
+  },
+  preview: {
+    host: '0.0.0.0', 
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+    allowedHosts: ['graphibooks.onrender.com'], 
+  },
 });
